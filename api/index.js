8cd7bbb282c6a69e5008c4ea0121b28da8c6587d
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2025-01-29 16:31:04 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -19,28 +19,33 @@ dbConnect();
 const app = express();
 app.use(express.json())
 app.use(cookieParser());
-// app.use(cors());
-app.use(
-  cors({
-      origin: "https://reat-estate-mern.vercel.app",
-      methods: "GET,POST,PUT,DELETE",
-      credentials: true,
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//       origin: "https://reat-estate-mern.vercel.app",
+//       methods: "GET,POST,PUT,DELETE",
+//       credentials: true,
+//   })
+// );
 
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
 })
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Backend');
+});
+
+
+
+
 app.use('/api/user', userroute);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 app.use('/api/rateus', reviewRatingRouter);
 
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Backend');
-});
 
 
 // app.use(express.static(path.join(__dirname, '/client/dist')));
